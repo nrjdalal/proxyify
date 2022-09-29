f809eadc1e0c1e5cc4e999999ac77c29f5917959
Neeraj Dalal <admin@nrjdalal.com>
2022-09-29 12:30:54 +0530

    latest code

--- app.js
+++ app.js
@@ -157,6 +157,7 @@ app.get('/', async (req, res) => {
         'Accept-Encoding': 'gzip, deflate, br',
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:105.0) Gecko/20100101 Firefox/105.0',
       },
+      timeout: 5000,
     })
     if (req.query.url.includes('amazon') && req.query.autoparse === 'true') {
       response = await response.text()
