cfea93087aa17fb62973ab810d3357b9ec08a069
Neeraj Dalal <admin@nrjdalal.com>
2022-09-30 22:29:20 +0530

    Fri Sep 30 22:29:20 IST 2022

--- app.js
+++ app.js
@@ -142,7 +142,7 @@ app.get('/', async (req, res) => {
       i,
       current[i % current.length].PublicIpAddress.split('.')[3],
       req.headers.host,
-      req.query.url.match(/[A-Z0-9]{10}/)[0]
+      req.query.url.match(/[A-Z0-9]{10}/)
     )
   }
 
